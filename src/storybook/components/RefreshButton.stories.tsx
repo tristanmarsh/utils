25304bd6373326b36ac28bf618d3cd91c04d0ff4
Tristan Marsh <tristanmarsh@live.com>
2020-08-14 00:09:23 +1000

    docs(storybook): add refresh button and motion default stories

---
+++ src/storybook/components/RefreshButton.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { RefreshButton } from './RefreshButton'
+
+export default {
+  title: 'RefreshButton',
+}
+
+export const Default = () => <RefreshButton onClick={() => {}} />
